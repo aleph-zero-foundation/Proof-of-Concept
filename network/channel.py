fd17ce38a4bf539fa2f4463a5498a9908cb43df1
Michal Handzlik <4hansu@gmail.com>
2018-10-18 11:25:29 +0200

    network package interface

---
+++ network/channel.py
@@ -0,0 +1,10 @@
+class Channel:
+
+    def __init__(self, ):
+        pass
+
+    def receive(self):
+        pass
+
+    def send(self, unit):
+        pass
---
+++ network/network.py
@@ -0,0 +1,12 @@
+class Network:
+
+    def __init__(self, addresses):
+        pass
+
+
+    def listen(self):
+        pass
+
+
+    def knock(self, process_id):
+        pass
