f6e4ef96376dc3e7d14435cd36b84b0cf533294f
Michael Swietek <michael.swietek@alephzero.org>
2018-10-26 16:02:48 +0200

    adds the level field to the unit class

--- data_structures/unit.py
+++ data_structures/unit.py
@@ -4,9 +4,9 @@
 class Unit(object):
     '''This class is the building block for the poset'''
 
-    __slots__ = ['creator_id', 'parents', 'txs', 'signature', 'coinshares']
+    __slots__ = ['creator_id', 'parents', 'txs', 'signature', 'coinshares', 'level']
 
-    def __init__(self, creator_id, parents, txs, signature, coinshares):
+    def __init__(self, creator_id, parents, txs, signature=None, coinshares=None, level=None):
         '''
         :param int creator_id: indentification number of a process creating this unit
         :param list parents: list of hashes of parent units; first parent has to be above a unit created by the process creator_id
@@ -19,6 +19,7 @@ class Unit(object):
         self.txs = txs
         self.signature = signature
         self.coinshares = coinshares
+        self.level = level
 
     def hash(self):
         '''
