e24560e96e04c783e0f96630b372ada6b0983993
Michael Swietek <michael.swietek@alephzero.org>
2018-11-13 14:34:48 +0100

    correctly plots posets without forks

--- aleph/utils/plot.py
+++ aleph/utils/plot.py
@@ -6,62 +6,70 @@ import matplotlib.pyplot as plt
 
 
 def parse_line(line):
-    node, creator_id = line.split()[:2]
+    unit, creator_id = line.split()[:2]
     parents = line.split()[2:]
 
-    return node, int(creator_id), parents
+    return unit, int(creator_id), parents
+
 
 def plot(path):
     '''
     :param string path: path to a file with poset description
+    Reads a description of a poset stored in path and draws a simple plot.
     '''
-    unit_creator = {}
+    creator, height = {}, {}
 
     n_processes, genesis_unit = None, None
-    x, units_per_process = None, None
+    units_per_process = None
+    x, y = None, None
     G = nx.DiGraph()
 
-
     with open(path, 'r') as f:
-
         n_processes, genesis_unit = f.readline().split()
         n_processes = int(n_processes)
+
         G.add_node(genesis_unit)
-        unit_creator[genesis_unit] = -1
+        height[genesis_unit] = -1
+        creator[genesis_unit] = -1
+
         units_per_process = [[] for _ in range(n_processes)]
+        # set x coordinates of units per process
         x = dict(zip(range(n_processes), np.linspace(27, 243, n_processes)))
 
         for line in f:
-            node, creator_id, parents = parse_line(line)
+            unit, creator_id, parents = parse_line(line)
 
-            unit_creator[node] = creator_id
-            units_per_process[creator_id].append(node)
+            creator[unit] = creator_id
+            units_per_process[creator_id].append(unit)
+            height[unit] = max([height[parent] for parent in parents]) + 1
 
-            G.add_node(node)
+            G.add_node(unit)
             for parent in parents:
-                G.add_edge(node, parent)
+                G.add_edge(unit, parent)
 
-    #pos = graphviz_layout(G, prog='dot')
-    # plt.figure(figsize=(30, 30))
+    # pos = graphviz_layout(G, prog='dot')
 
-    pos = {'G':(135,10)}
+    # find positions of units in the plot
+    # we plot units created by a given process veritcally
+    # we use height[unit] for its height in the plot
+    # TODO plot forks next to each other
+    pos = {genesis_unit: (135, 10)}
     for pid in range(n_processes):
         err = 10 * np.random.rand(len(units_per_process[pid]))
-        spaces = 60*np.arange(len(units_per_process[pid])) + err + 70
-        print(spaces)
+        spaces = 60*np.array([height[unit] for unit in units_per_process[pid]]) + err + 70
         y = dict(zip(units_per_process[pid], spaces))
 
         for unit in units_per_process[pid]:
-            pos[unit] = (x[unit_creator[unit]], y[unit])
+            pos[unit] = (x[creator[unit]], y[unit])
 
     color_values = np.linspace(0, 1, n_processes+1)[1:]
     color_map = dict(zip(range(n_processes), color_values))
     color_map[-1] = 0
-    print(color_map)
-    node_color = [color_map[unit_creator[node]] for node in G.nodes()]
-    nx.draw(G, pos, with_labels=True, arrows=True, node_color=node_color)
+    node_color = [color_map[creator[unit]] for unit in G.nodes()]
+    nx.draw(G, pos, with_labels=True, arrows=True, node_color=node_color, node_size=1000, cmap=plt.get_cmap('jet'))
     plt.show()
 
 
 if __name__ == '__main__':
-    plot('simple_graph')
+    plot('random_10_30.txt')
+    # plot('simple_graph')
