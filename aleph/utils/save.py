07c69f2bf4274d9239d1f7b8521bbca6a03592f2
Michal Handzlik <4hansu@gmail.com>
2018-11-13 12:01:34 +0100

    Working save() implementation

--- aleph/utils/save.py
+++ aleph/utils/save.py
@@ -1,41 +1,42 @@
 def topsort(poset):
+    children = {}
     for U in poset.units.values():
-        U.children = 0
+        children[U] = 0
 
     for U in poset.units.values():
         for parent in U.parents:
-            parent.children += 1
+            children[parent] += 1
 
     childless = []
     for U in poset.units.values():
-        if U.children == 0:
+        if children[U] == 0:
             childless.append(U)
 
     ret = []
     while childless:
         U = childless.pop()
-        del U.children
         ret.append(U)
         for parent in U.parents:
-            parent.children -= 1
-            if parent.children == 0:
+            children[parent] -= 1
+            if children[parent] == 0:
                 childless.append(parent)
-    return ret
+    return list(reversed(ret))
 
 
 def save(poset, filename, genesis='G'):
     toporder = topsort(poset)
-    assert toporder[-1] is poset.genesis_unit, "Genesis U is not last in topological order"
+    assert toporder[0] is poset.genesis_unit, "Genesis U is not last in topological order"
 
-    for i, U in enumerate(toporder):
-        U.name = i
-    toporder[-1].name = genesis
+    names = {}
+    names[toporder[0]] = genesis
+    for i, U in enumerate(toporder[1:]):
+        names[U] = i
 
     with open(filename, 'w') as f:
         f.write('{} {}\n'. format(poset.n_processes, genesis))
-        for U in toporder:
-            line = '{} {}'.format(U.name, U.creator_id)
+        for U in toporder[1:]:
+            line = '{} {}'.format(names[U], U.creator_id)
             for parent in U.parents:
-                line += ' {}'.format(parent.name)
+                line += ' {}'.format(names[parent])
             line += '\n'
             f.write(line)
