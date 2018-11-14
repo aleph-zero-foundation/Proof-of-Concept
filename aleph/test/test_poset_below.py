f79a65db41b2f5b220934ee0b103247027d139af
Damian Straszak <damian.straszak@gmail.com>
2018-11-14 11:28:58 +0100

    minor changes to test_poset_below.py

--- aleph/test/test_poset_below.py
+++ aleph/test/test_poset_below.py
@@ -1,7 +1,7 @@
 from aleph.data_structures import Unit, Poset
 from aleph.utils import dag_utils
 
-def test_create_poset():
+def test_trivial_single_level_below():
     n_processes = 4
     genesis_unit = Unit(creator_id = None, parents = [], txs = [])
     poset = Poset(n_processes = n_processes, process_id = 0, genesis_unit = genesis_unit,
@@ -16,7 +16,13 @@ def test_create_poset():
         
     U0 = poset.max_units_per_process[0]
     U1 = poset.max_units_per_process[1]
+    U2 = poset.max_units_per_process[2]
+    U3 = poset.max_units_per_process[3]
+    
     U = Unit(creator_id = 0, parents = [U0, U1], txs = [])
     poset.add_unit(U)
-    assert poset.below(U0,U)
-    assert poset.above(U,U0)
+    assert poset.below(U0, U)
+    assert poset.above(U, U0)
+    assert poset.below(U1, U)
+    assert not poset.below(U2, U)
+    assert not poset.below(U3, U)
