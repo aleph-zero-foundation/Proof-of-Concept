37cc0cd67373b5dd498b3ed917fb563ed0c4c5bd
Michael Swietek <michael.swietek@alephzero.org>
2018-11-17 18:51:52 +0100

    plot from dag, still no forks allowed

--- aleph/utils/plot.py
+++ aleph/utils/plot.py
@@ -4,6 +4,7 @@ from networkx.drawing.nx_agraph import graphviz_layout
 
 import matplotlib.pyplot as plt
 
+from aleph.utils.dag_utils import topological_sort
 
 def parse_line(line):
     unit, creator_id = line.split()[:2]
@@ -13,11 +14,43 @@ def parse_line(line):
     return unit, int(creator_id), parents
 
 
-def plot(path):
+def plot(dag, n_processes):
     '''
-    :param string path: path to a file with poset description
-    Reads a description of a poset stored in path and draws a simple plot.
+    :param dict dag: (unit, creator_id) -> [unit_parents]
     '''
+
+    G = nx.DiGraph()
+    height, creator = {}, {}
+
+    for unit, creator_id in topological_sort(dag):
+        G.add_node(unit)
+        for parent, _ in dag[(unit, creator_id)]:
+            G.add_edge(unit, parent)
+        print(unit)
+        height[unit] = max([height[parent] for parent, _ in dag[(unit, creator_id)]], default=-1) + 1
+        creator[unit] = creator_id
+
+    pos = {}
+
+    x = dict(zip(range(n_processes), np.linspace(27, 243, n_processes)))
+    for pid in range(n_processes):
+        units_per_pid = [unit for (unit, creator_id) in dag.keys() if creator_id == pid]
+        err = 10 * np.random.rand(len(units_per_pid))
+        spaces = 60*np.array([height[unit] for unit in units_per_pid]) + err + 70
+        y = dict(zip(units_per_pid, spaces))
+
+        for unit in units_per_pid:
+            pos[unit] = (x[pid], y[unit])
+
+    color_values = np.linspace(0, 1, n_processes+1)[1:]
+    color_map = dict(zip(range(n_processes), color_values))
+    node_color = [color_map[creator[unit]] for unit in G.nodes()]
+    nx.draw(G, pos, with_labels=True, arrows=True, node_color=node_color, node_size=1000, cmap=plt.get_cmap('jet'))
+    plt.show()
+
+
+
+def pl(path):
     creator, height = {}, {}
     self_ancestor, self_predecessor = {}, {}
 
@@ -101,5 +134,7 @@ def plot(path):
 
 
 if __name__ == '__main__':
-    plot('random_forking_6_20.txt')
-    # plot('simple_graph')
+    from aleph.utils.dag_utils import dag_from_file
+    path = 'aleph/test/examples/random_10_30.txt'
+    dag, n_processes = dag_from_file(path)
+    plot(dag, n_processes)
