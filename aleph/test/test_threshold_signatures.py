16b5d172b25cbbd72c1ab8fc787ddd6188d6d431
Michael Swietek <michael.swietek@alephzero.org>
2018-11-20 18:14:03 +0100

    initial threshold signatures implementation

---
+++ aleph/test/test_threshold_signatures.py
@@ -0,0 +1,23 @@
+from aleph.crypto.threshold_coin.threshold_signatures import generate_keys
+from random import sample
+
+class TestThresholdSignatures():
+    def test_one(self):
+        n_parties, threshold = 10, 5
+        VK, SKs = generate_keys(n_parties, threshold)
+
+        msg = 'there is no spoon'
+        msg_hash = VK.hash_msg(msg)
+
+        # generate signature shares of all parties
+        shares = [SK.generate_share(msg_hash) for SK in SKs]
+        _shares = {i:shares[i] for i in sample(range(n_parties), threshold)}
+
+        # check if all shares are valid
+        for i, share in _shares.items():
+            assert VK.verify_share(share, i, msg_hash)
+
+        # combine shares and check if the signature is valid
+        signature = VK.combine_shares(_shares)
+
+        assert VK.verify_signature(signature, msg_hash)
