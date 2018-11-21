60753ea5ab97590941705483ed17094fe51b56c0
Michal Handzlik <michal.handzlik@alephzero.org>
2018-11-21 14:50:48 +0100

    Add memo decorator to DAG

--- aleph/utils/dag.py
+++ aleph/utils/dag.py
@@ -1,6 +1,31 @@
+import collections
 import copy
+import functools
 
 from aleph.data_structures import Poset, Unit
+
+
+class memo(object):
+    '''Decorator. Caches a function's return value each time it is called.
+    If called later with the same arguments, the cached value is returned
+    (not reevaluated).
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
+
 
 
 class DAG:
@@ -25,6 +50,7 @@ class DAG:
         self.nodes[name] = parents[:]
 
 
+    #@memo
     def is_reachable(self, U, V):
         '''Checks whether V is reachable from U in a DAG, using BFS
         :param dict dag: a dictionary of the form: node -> [list of parent nodes]
@@ -66,14 +92,19 @@ class DAG:
         return ret
 
 
-    def is_reachable_through_n(self, U, V, n):
+    #@memo
+    def count_support(self, U, V):
+        below_V = self.nodes_below(V)
+        above_U = self.reverse().nodes_below(U)
+        return len({self.pid(node) for node in (below_V & above_U)})
+
+
+    def is_reachable_through_n_intermediate(self, U, V, n):
         '''
         Checks whether the number of different processes that have units on some path
         from U to V in dag is at least n.
         '''
-        below_V = self.nodes_below(V)
-        above_U = self.reverse().nodes_below(U)
-        return len({self.pid(node) for node in (below_V & above_U)}) >= n
+        return self.count_support(U, V) >= n
 
 
     def self_predecessor(self, pid, parent_nodes):
@@ -92,7 +123,7 @@ class DAG:
         for U in subset:
             is_maximal = True
             for V in subset:
-                if V is not U and self.below(U, V):
+                if V is not U and self.is_reachable(U, V):
                     is_maximal = False
                     break
             if is_maximal:
