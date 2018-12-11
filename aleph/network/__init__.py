d7a992d20914602c56234bfffe36609f0fafa80a
Michael Swietek <michael.swietek@alephzero.org>
2018-12-11 14:57:21 +0100

    adds tx_generator that sends txs over socket

--- aleph/network/__init__.py
+++ aleph/network/__init__.py
@@ -1,1 +1,2 @@
 from .network import listener, sync, tx_listener
+from .tx_generator import tx_generator
---
+++ aleph/network/tx_generator.py
@@ -0,0 +1,41 @@
+import marshal
+import random
+import socket
+import time
+
+from aleph.crypto.keys import SigningKey, VerifyKey
+from aleph.data_structures import tx_to_message
+
+
+def tx_generator(committee_addresses, n_light_nodes, txps):
+    last_tx_index = [-1 for _ in range(n_light_nodes)]
+    signing_keys = [SigningKey() for _ in range(n_light_nodes)]
+    verify_keys_hex = [VerifyKey.from_SigningKey(sk).to_hex() for sk in signing_keys]
+
+    counter = 0
+    starts = time.time()
+
+    while True:
+        if counter == txps:
+            counter = 0
+            if time.time() - starts < 1:
+                time.sleep(1-(time.time()-starts))
+            starts = time.time()
+
+        com_addr = random.choice(committee_addresses)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect(com_addr)
+
+            issuer_id = random.randrange(0, n_light_nodes)
+            receiver_id = random.choice([uid for uid in range(n_light_nodes) if uid != issuer_id])
+            tx = {'issuer': verify_keys_hex[issuer_id],
+                  'amount': random.randrange(1, 10000),
+                  'receiver': verify_keys_hex[receiver_id],
+                  'index': last_tx_index[issuer_id] + 1}
+            message = tx_to_message(tx['issuer'], tx['amount'], tx['receiver'], tx['index'])
+            tx['signature'] = signing_keys[issuer_id].sign(message)
+
+            data = marshal.dumps(tx)
+            sock.sendall(data)
+            counter += 1
