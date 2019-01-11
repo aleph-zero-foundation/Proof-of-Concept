74d405df405b469e23376c5924bf618e1e736b6a
Damian <damian.straszak@gmail.com>
2019-01-11 12:32:34 +0100

    Added bounded caching to crp.

--- aleph/crypto/crp.py
+++ aleph/crypto/crp.py
@@ -2,6 +2,7 @@ from .xor import xor
 
 import hashlib
 
+
 class CommonRandomPermutation:
 
     def __init__(self, public_keys_hex, hashing_function = None):
@@ -12,6 +13,9 @@ class CommonRandomPermutation:
         """
         self.public_keys_hex = public_keys_hex
         self.hashing_function = hashing_function
+        self.cache = {}
+        self.cache_size = 20
+
 
     def _hash(self, bytestring):
         if self.hashing_function is None:
@@ -19,8 +23,20 @@ class CommonRandomPermutation:
         else:
             return self.hashing_function(bytestring)
 
+    def add_to_cache(self, k, permutation):
+        # adds an element to cache and shrinks the size of cache if the size becomes > 2*cache_size
+        self.cache[k] = permutation
+        if len(self.cache) > 2*self.cache_size:
+            # the cache grew too large -- fetch its elements sorted from highest indices to lowest
+            cache_items = list(reversed(sorted(self.cache.items())))
+            # remove all but cache_size largest indices
+            self.cache = dict(cache_items[:self.cache_size])
+
     def __getitem__(self, k):
         """Return k-th common random permutation."""
+        if k in self.cache:
+            return self.cache[k]
+
         xor_all = bytes([0])
         for pk in self.public_keys_hex:
             xor_all = xor(xor_all, pk)
@@ -30,5 +46,7 @@ class CommonRandomPermutation:
 
         indexed_seeds = zip(seeds, range(len(seeds)))
         indexed_seeds = sorted(list(indexed_seeds))
-        return [ind for (seed, ind) in indexed_seeds]
+        permutation = [ind for (seed, ind) in indexed_seeds]
+        self.add_to_cache(k, permutation)
+        return permutation
 
