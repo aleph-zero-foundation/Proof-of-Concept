5cb4e1261dddbc65a5348032e0c9440f6dd7ad57
Michael Swietek <michael.swietek@alephzero.org>
2019-01-17 17:41:42 +0100

    adds initilization of ec2 machine

--- experiments/aws/fabfile.py
+++ experiments/aws/fabfile.py
@@ -1,9 +1,21 @@
 from fabric import task
 
+
+@task
+def test(conn):
+    conn.run('uname -a')
+
+
 @task
 def run_tests(conn):
     with conn.cd('proof-of-concept'):
-        conn.run('pytest-3 aleph')
+        conn.run('pytest aleph')
+
+
+@task
+def sync_keys(conn):
+    #conn.put()
+    pass
 
 
 @task
@@ -12,7 +24,7 @@ def install_dependencies(conn):
 
     conn.sudo('apt install -y make flex bison libgmp-dev libmpc-dev libssl-dev')
     conn.sudo('apt install -y python3-dev python3-pip')
-    conn.sudo('pip3 install setuptools')
+    conn.sudo('pip3 install setuptools pytest-xdist')
 
     conn.run('wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz')
     conn.run('tar -xvf pbc-0.5.14.tar.gz')
@@ -27,6 +39,21 @@ def install_dependencies(conn):
         conn.run('make')
         conn.run('sudo make install')
 
-    conn.run('git clone https://gitlab.com/mike1729/proof-of-concept.git')
+
+@task
+def clone_repo(conn):
+    conn.run('git clone http://gitlab+deploy-token-38770:usqkQKRbQiVFyKZ2byZw@gitlab.com/alephledger/proof-of-concept.git')
     with conn.cd('proof-of-concept'):
+        conn.run('git checkout devel')
         conn.run('sudo pip3 install -e .')
+
+@task
+def init(conn):
+    sync_keys(conn)
+    install_dependencies(conn)
+    clone_repo(conn)
+
+
+@task
+def stop_world(conn):
+    pass
