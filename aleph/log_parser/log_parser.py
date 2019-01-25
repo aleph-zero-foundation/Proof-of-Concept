2f1ceb54acd3f51a87005ca9d7515d18731775a1
Damian <damian.straszak@gmail.com>
2019-01-25 11:07:03 +0100

    Basic pipeline for parsing log messages established in log_parser.py

--- aleph/log_parser/log_parser.py
+++ aleph/log_parser/log_parser.py
@@ -1,41 +1,73 @@
 from parse import parse, compile
+from datetime import datetime
 
 pattern =  compile("[{date}] [{level}] [{name}] {msg} [{file_line}]")
 split_on_bar = compile("{left}|{right}")
 
 
-def get_tokens(space_separated_string)
+def get_tokens(space_separated_string):
     return [s.strip() for s in space_separated_string.split()]
 
 
+def parse_create(ev_type, ev_params, msg_body, event):
+    event['process_id'] = int(ev_params[0])
+    pattern_create = compile("Created a new unit <{unit}>")
+    event['units'] = pattern_create.search(msg_body)['unit']
+
+def parse_mem_usg(ev_type, ev_params, msg_body, event):
+    event['process_id'] = int(ev_params[0])
+    pattern_memory = compile("{usage:d} MiB")
+    event['memory'] = pattern_memory.search(msg_body)['usage']
+
+
 def event_from_log_line(line):
-    res =  pattern.parse(line)
-    assert res is not None
-    event = res.named
+    parsed_line =  pattern.parse(line)
+    assert parsed_line is not None
+    event = parsed_line.named
 
     msg = event['msg']
-    res = split_on_bar.parse(msg)
-    assert res is not None, "split on bar failed"
-    event_type, body = res.named
+    split_msg = split_on_bar.parse(msg)
+    if split_msg is None:
+        # this happens when some log message is not formatted using "|"
+        # it means we should skip it
+        return None
+    event_descr, msg_body = split_msg['left'].strip(), split_msg['right'].strip()
+    #print(event_descr)
 
+    ev_tokens = get_tokens(event_descr)
+    ev_type = ev_tokens[0]
 
-
-
-
-
+    event_handler = {
+        'create_add': parse_create,
+        'memory_usage': parse_mem_usg,
+        }
+    if ev_type in event_handler:
+        event_handler[ev_type](ev_type, ev_tokens[1:], msg_body, event)
+    else:
+        return None
+    event.pop('msg', None)
+    event.pop('level', None)
+    event.pop('name', None)
+    event['date'] = datetime.strptime(event['date'], "%Y-%m-%d %H:%M:%S,%f")
     return event
+
+
 
 
 def parse_log_file(file_path):
     cnt = 0
+    evs = []
     with open(file_path, "r") as log_file:
         for line in log_file:
             cnt += 1
             e = event_from_log_line(line.strip())
-            if cnt == 1000:
-                print(e)
-
-    print(cnt)
+            if e is not None:
+                evs.append(e)
+            #if cnt == 1000:
+            #    print(e)
+    for e in evs:
+        print(e)
+    #print(cnt)
 
 
 path = r'../../tests/aleph.log'
