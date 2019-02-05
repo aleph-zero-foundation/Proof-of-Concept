a948fc3148fc16909318ef3e325acc965a2caf49
Michael Swietek <michael.swietek@alephzero.org>
2019-02-05 14:16:37 +0100

    splits config into config and const

--- aleph/config.py
+++ aleph/config.py
@@ -1,19 +1,4 @@
-N_PARENTS = 2
-N_CREATE = 2  # number of new units per second
-CREATE_FREQ = 1/N_CREATE  # frequency of creating new units
-SYNC_INIT_FREQ = CREATE_FREQ # frequency of initianing syncs with other processes
-SIGNING_FUNCTION = None  # TODO pick one!
-N_TXS = 10000  # number of transactions per unit
-
-HOST_IP = '127.0.0.1'
-HOST_PORT = 8888
-
-N_RECV_SYNC = 5
-LOGGER_NAME = 'aleph'
-
-ADD_SHARES = 4
-
-SEND_COMPRESSED = True # use zlib compressing/decompressing when sending data over the network
+from aleph.const import LOGGER_NAME
 
 import charm.toolbox.pairinggroup
 PAIRING_GROUP = charm.toolbox.pairinggroup.PairingGroup('MNT224')
@@ -24,7 +9,6 @@ GENERATOR.initPP()
 
 import pickle
 pickle.DEFAULT_PROTOCOL = 4
-
 
 import logging
 log_format = '[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s [%(filename)s:%(lineno)d]'
