57091e85501902e9cad2c88862d1b81b09762d27
Damian <damian.straszak@gmail.com>
2019-02-09 19:08:11 +0100

    Fixed tests to use Poset by default and not FastPoset.

--- aleph/utils/generic_test.py
+++ aleph/utils/generic_test.py
@@ -47,8 +47,7 @@ def generate_crp(n_processes):
 
 def initialize_posets(n_processes, use_tcoin = False):
     crp = generate_crp(n_processes)
-    if use_tcoin:
-        return [Poset(n_processes, process_id, crp, use_tcoin = use_tcoin) for process_id in range(n_processes)]
+    return [Poset(n_processes, process_id, crp, use_tcoin = use_tcoin) for process_id in range(n_processes)]
 
 def distribute_unit(U, name, posets, forkers, name_to_unit, unit_to_name):
     for process_id in range(len(posets)):
