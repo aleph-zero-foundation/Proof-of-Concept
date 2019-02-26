aa27c5c2ced83e27557685a90bbcca0d0a9ef312
Damian <damian.straszak@gmail.com>
2019-02-26 17:25:29 +0100

    Moved files related to analyzing logs to a common directory.

---
+++ aleph/log_analyzer/run_analyzer.py
@@ -0,0 +1,108 @@
+from aleph.log_analyzer import LogAnalyzer
+import os
+import sys
+
+def prepare_common_stats(process_stats, rep_dir):
+    '''
+    Write basic stats common to all processes to a file.
+    '''
+    # NOTE: the txps in this stat is calculated as the ratio:
+    #            (total n of txs validated by timing units up to lvl L) / (time to find the timing unit at lvl L)
+    # where L is the highest level for which a timing unit has been established
+
+    n_stats = len(process_stats)
+    rep_path = os.path.join(rep_dir, "common-stats.txt")
+    with open(rep_path, "w") as rep_file:
+
+        fields = ['latency', 'txps']
+        header = ''.join( s.ljust(20) for s in ['name', 'median', 'min (proc_id)', 'max (proc_id)'])
+        print(header)
+        rep_file.write(header + '\n')
+        for field in fields:
+            process_stats.sort(key = lambda x: x[field])
+            line = field.ljust(20)
+            median = process_stats[n_stats//2][field]
+            line += (f'{median:.3f}').ljust(20)
+            min_val, min_proc = process_stats[0][field], process_stats[0]['process_id']
+            line += (f'{min_val:.3f} ({min_proc})').ljust(20)
+            max_val, max_proc = process_stats[-1][field], process_stats[-1]['process_id']
+            line += (f'{max_val:.3f} ({max_proc})').ljust(20)
+            print(line)
+            rep_file.write(line + '\n')
+
+
+def parse_args_and_run():
+
+    if len(sys.argv) < 2:
+        print("Need to provide arguments. Try one of:")
+        print("1) Log_file_path     (In case the log file contains data from one process only)")
+        print("   Ex: python run_analyzer.py aleph.log")
+        print("2) Log_file_path process_id           (In case the log file contains data from multiple processes)")
+        print("   Ex: python run_analyzer.py aleph.log 0")
+        print("3) ALL Dir_with_multiple_logs Destination_dir")
+        print("   Ex: python run_analyzer.py ALL logs reports")
+        sys.exit(0)
+
+    if len(sys.argv) == 4 and sys.argv[1] == 'ALL':
+
+        log_dir = sys.argv[2]
+        rep_dir = sys.argv[3]
+
+        if not os.path.isdir(log_dir):
+            print(f"No such directory {log_dir}.")
+            sys.exit(0)
+
+        if not os.path.isdir(rep_dir):
+            print(f"No such directory {rep_dir}. Creating.")
+            os.mkdir(rep_dir)
+
+        list_logs = os.listdir(log_dir)
+        # do not parse other.log etc.
+        list_logs = sorted([log_file for log_file in list_logs if os.path.basename(log_file).find("aleph") != -1])
+        process_stats = []
+        for ind, log_name in enumerate(list_logs):
+
+            path = os.path.join(log_dir, log_name)
+            print(f'Analyzing {path}...')
+            if ind == 0:
+                generate_plots = True
+                print('Will generate plots only for this log.')
+            else:
+                generate_plots = False
+
+            analyzer = LogAnalyzer(path, generate_plots = generate_plots)
+            if not analyzer.analyze():
+                print('Failed because the log does not even contain the Process start message.')
+                continue
+            process_id = analyzer.process_id
+            print(f"{ind}: Process' {process_id} log analyzed.\n")
+            analyzer.prepare_basic_report(rep_dir)
+            analyzer.prepare_report_per_process(rep_dir)
+
+            stats = {'process_id' : process_id}
+            stats['latency'] = analyzer.get_unit_latency()
+            stats['txps'] = analyzer.get_txps_till_last_timing_unit()
+            process_stats.append(stats)
+
+        prepare_common_stats(process_stats, rep_dir)
+
+
+    if len(sys.argv) in [2,3]:
+        path = sys.argv[1]
+        if len(sys.argv) == 3:
+            process_id = int(sys.argv[2])
+        else:
+            process_id = None
+
+        analyzer = LogAnalyzer(path, process_id, generate_plots = True)
+        if not analyzer.analyze():
+            print('Failed because the log does not even contain the Process start message.')
+            sys.exit(0)
+        process_id = analyzer.process_id
+        analyzer.prepare_basic_report('.')
+        analyzer.prepare_report_per_process('.')
+
+
+if __name__ == '__main__':
+    parse_args_and_run()
+
