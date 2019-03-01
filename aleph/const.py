2878c845cb4edbae15e7d5fcb1fc5d25ee93bb82
Michael Swietek <michael.swietek@alephzero.org>
2019-03-01 10:26:42 +0100

    removes logging USE_FAST_POSET

--- aleph/const.py
+++ aleph/const.py
@@ -1,21 +1,21 @@
-N_PARENTS       = 2            # maximal number of parents a unit can have
+N_PARENTS       = 10           # maximal number of parents a unit can have
 USE_MAX_PARENTS = 1            # prefer maximal units (globally maximal in poset) when choosing parents
 
 VOTING_LEVEL    = 3            # level at which the first voting round occurs, this is "t" from the write-up
 PI_DELTA_LEVEL  = 12           # level at which to switch from the "fast" to the pi_delta algorithm
-USE_TCOIN       = 0            # whether to use threshold coin
+USE_TCOIN       = 1            # whether to use threshold coin
 ADD_SHARES      = PI_DELTA_LEVEL - 1   # level at which to start adding coin shares to units, it's safe to make it PI_DELTA_LEVEL - 1
                                        # keeping it here for the purpose of tests, for which we might want to set it to a lower value
 
 CREATE_FREQ     = 1.0          # frequency of creating new units
-SYNC_INIT_FREQ  = 1.0          # frequency of initianing syncs with other processes
+SYNC_INIT_FREQ  = 0.5          # frequency of initianing syncs with other processes
 N_RECV_SYNC     = 10           # number of allowed parallel received syncs
-N_INIT_SYNC     = 5            # number of allowed parallel initiated syncs
+N_INIT_SYNC     = 10           # number of allowed parallel initiated syncs
 
-TXPU            = 2000         # number of transactions per unit
+TXPU            = 10           # number of transactions per unit
 TX_LIMIT        = 1000000
 
-LEVEL_LIMIT     = 100          # maximal level after which process shuts down
+LEVEL_LIMIT     = 20           # maximal level after which process shuts down
 UNITS_LIMIT     = None         # maximal number of units that are constructed
 SYNCS_LIMIT     = None         # maximal number of syncs that are performed
 
