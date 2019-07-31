402458b995f49c7910c0b58c4eeef279ba6e57fc
Michal Handzlik <michal.handzlik@alephzero.org>
2019-07-31 11:48:27 +0200

    tx.py

--- aleph/data_structures/tx.py
+++ aleph/data_structures/tx.py
@@ -1,5 +1,5 @@
 class Tx(object):
-    '''This class stores a transactions issued by some user'''
+    '''A class representing a single transaction, that is an act of sending some number of tokens from one user to another.'''
 
     __slots__ = ['issuer', 'receiver', 'amount']
 
@@ -13,11 +13,14 @@ class Tx(object):
         self.receiver = receiver
         self.amount = amount
 
+
     def __getstate__(self):
         return (self.issuer, self.receiver, self.amount)
 
+
     def __setstate__(self, state):
         self.issuer, self.receiver, self.amount = state
+
 
     def __str__(self):
         tx_string =  'Issuer: ' + str(self.issuer) + '\n'
@@ -27,10 +30,12 @@ class Tx(object):
 
     __repr__ = __str__
 
+
     def __eq__(self, other):
         return (isinstance(other, Tx) and self.issuer == other.issuer
                 and self.receiver == other.receiver
                 and self.amount == other.amount)
+
 
     def __hash__(self):
         return hash(str(self))
